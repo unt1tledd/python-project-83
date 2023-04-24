b4829f86cd50e05274768e4d2e8186a013d770d5
Софья Григорович <sonyashkin@icloud.com>
2023-04-24 20:10:04 +0300

    st3

--- database.sql
+++ database.sql
@@ -1,11 +1,15 @@
+DROP TABLE IF EXISTS checks;
+DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
 	id			bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name		varchar(255) UNIQUE NOT NONE,
 	created_at	timestamp
 );
 
-CREATE TABLE info (
+CREATE TABLE checks (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	url_id bigint REFERENCES urls(id),
 	request_code integer,
 	h1 varchar(255),
 	title varchar,
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,11 @@
 import os
 import psycopg2
 import psycopg2.extras
+import datetime
+import jinja2
 from flask import Flask, request, url_for, flash, redirect, render_template
 from page_analyzer.validator import validate
+from requests import ConnectionError, HTTPError
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
@@ -25,14 +28,14 @@ def index():
 
 
 @app.post('/urls')
-def post_urls():
+def post_url():
     url = request.form.get('url')
     errors = validate(url)
     
     if errors:
         for error in errors:
-            flash(error)
-        return render_template('index.html', url=url, errors=errors), 422
+            flash(error, "alert alert-danger")
+        return render_template('index.html', url=url), 422
 
     parse_url = urlparse(url)
     valid_url = parse_url.scheme + '://' + parse_url.netloc
@@ -41,10 +44,10 @@ def post_urls():
             cur.execute("""
                 SELECT id FROM urls
                 WHERE name = %s""", [valid_url])
-            result = cur.fetchone()[0]
+            result = cur.fetchone()
             if result:
                 flash('Страница ужу существует', 'alert alert-info')
-                return redirect(url_for('added_url', id=result))
+                return redirect(url_for('added_url', id=result.id))
     
     with get_connection() as conn:
         with conn.cursor() as cur:
@@ -58,7 +61,7 @@ def post_urls():
         return redirect(url_for('added_url', id=result))
 
 
-@app.get('/urls/<id>')
+@app.route('/urls/<id>')
 def added_url(id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
@@ -68,14 +71,61 @@ def added_url(id):
             url_name, url_created_at = cur.fetchone()
     
     with get_connection() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
             cur.execute("""
-                SELECT request_cod, h1, title, discription, created_at info
+                SELECT request_cod, h1, title, discription, created_at FROM checks
                 WHERE id = %s ORDER BY DESC""", [id])
             result = fetchall()
-        return render_template(
-                'page.html',
-                url_name=url_name,
-                url_id=id,
-                url_created_at=url_created_at.date(),
-                checks=result)
+    return render_template(
+            'page.html',
+            url_name=url_name,
+            url_id=id,
+            url_created_at=url_created_at.date(),
+            checks=result)
+
+
+@app.get('/urls')
+def get_urls():
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+            cur.execute("""
+                SELECT
+                DISTINCT ON (urls.id) urls.id, urls.name, MAX(checks.created_at), checks.status_code
+                FROM urls
+                LEFT JOIN checks ON urls.id = checks.url_id
+                GROUP BY urls.id, checks.status_code
+                ORDER BY urls.id DESC""")
+            checks = cur.fetchall()
+    return render_template('pages.html', checks=checks)
+
+
+@app.route('/urls/<id>/checks', methods=['POST'])
+def id_check(id):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+            cur.execute("""
+                SELECT name
+                FROM urls
+                WHERE id = %s""", [id])
+            result = cur.fetchone()
+
+    url_name = result.name
+    try:
+        response = requests.get(url_name)
+        response.raise_for_status()
+    except (ConnectionError, HTTPError):
+        flash("Произошла ошибка при проверке", "alert alert-danger")
+        return redirect(url_for('url_added', id=id))
+
+    status_code = response.status_code
+    h1, title, meta = get_content_of_page(response.text)
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            date = datetime.date.today()
+            cur.execute("""
+                INSERT INTO checks (url_id, created_at, status_code, h1, title, description)
+                VALUES (%s, %s, %s, %s, %s, %s)""", [
+                id, date, status_code, h1, title, meta])
+            conn.commit()
+    flash("Страница успешно проверена", "alert alert-success")
+    return redirect(url_for('url_added', id=id)
